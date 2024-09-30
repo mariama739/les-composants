5961606d31b1e9033f10f6d4f2208114813fa3e7
mariama739 <mariamacoly739@gmail.com>
2024-09-30 13:34:13 +0000

    images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,8 @@
 // import exampleImage from ""; // Importez l'image
+import Image1 from '../assets/cacti-1846147_960_720.jpg';
+import Image2 from '../assets/istockphoto-1499045643-1024x1024.jpg';
+import Image3 from '../assets/plant-8234767_1280.webp';
+
 import { FaStar } from "react-icons/fa";
 
 function Moncarte() {
@@ -23,11 +27,7 @@ function Moncarte() {
             <div className="row p-4">
               <div className="col-md-4">
                 <div className="card" /*style="width: 18rem;"*/>
-                  <img
-                    src="./images/cacti-1846147_960_720.jpg  "
-                    className="card-img-top"
-                    alt="..."
-                  />
+                  <img src={Image1} className="card-img-top" alt="..." />
                   <div className="card-body">
                     <h5 className="card-title">Saint Louis</h5>
                     <p className="card-text text-primary">
@@ -44,11 +44,7 @@ function Moncarte() {
               </div>
               <div className="col-md-4">
                 <div className="card">
-                  <img
-                    src="./images/istockphoto-1499045643-1024x1024.jpg"
-                    className="card-img-top"
-                    alt="..."
-                  />
+                  <img src={Image2} className="card-img-top" alt="..." />
                   <div className="card-body">
                     <h5 className="card-title">Kolda</h5>
                     <p className="card-text text-primary">
@@ -66,11 +62,7 @@ function Moncarte() {
               </div>
               <div className="col-md-4">
                 <div className="card" /*style="width: 18rem;"*/>
-                  <img
-                    src="./images/plant-8234767_1280.webp"
-                    className="card-img-top"
-                    alt="..."
-                  />
+                  <img src={Image3} className="card-img-top" alt="..." />
                   <div className="card-body">
                     <h5 className="card-title">Matam</h5>
                     <p className="card-text text-primary">
